a8c12a4005d2db94e4edaf45302617003b893cdc
Alex Coleman <alexjcoleman@gmail.com>
2024-04-20 14:59:29 +0100

    Simplify playwright test

--- src/test/java/com/coleman/polls/e2e/TestHomePage.java
+++ src/test/java/com/coleman/polls/e2e/TestHomePage.java
@@ -1,9 +1,8 @@
 package com.coleman.polls.e2e;
 
-import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.junit.UsePlaywright;
 import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +11,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@UsePlaywright
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestHomePage {
 
@@ -19,18 +19,13 @@ public class TestHomePage {
   private int port;
 
   @Test
-  public void testHomeShowsHelloWorld() {
-    try (Playwright playwright = Playwright.create()) {
-      Browser browser = playwright.chromium().launch();
-      Page page = browser.newPage();
-      page.navigate("http://localhost:" + port + "/");
+  public void testHomeShowsHelloWorld(Page page) {
+    page.navigate("http://localhost:" + port + "/");
 
-      Locator heading = page.getByRole(AriaRole.HEADING);
+    Locator heading = page.getByRole(AriaRole.HEADING);
 
-      assertEquals(heading.textContent(), "Hello world!");
-    }
-
+    assertEquals(heading.textContent(), "Hello world!");
   }
 
-
 }
+
