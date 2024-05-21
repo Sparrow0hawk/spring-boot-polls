548316790dfb6b675e0c6f955665c6514e1127bb
Alex Coleman <alexjcoleman@gmail.com>
2024-05-21 20:45:16 +0100

    Rename Application class

--- src/main/java/com/coleman/polls/Application.java
+++ src/main/java/com/coleman/polls/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PollsApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(PollsApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
